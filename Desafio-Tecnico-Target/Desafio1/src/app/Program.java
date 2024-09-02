0714636d622719b20bd3648e9920e385110bfd5c
GabrielSz2 <gsouzaribeiro21@gmail.com>
2024-09-01 22:55:27 -0300

    Desafio completo

---
+++ Desafio-Tecnico-Target/Desafio1/src/app/Program.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		int indice = 13, soma = 0, K = 0;
+		
+		while(K < indice) {
+			K += 1;
+			soma += K;
+		}
+		
+		System.out.println(soma);
+		
+	}
+}
